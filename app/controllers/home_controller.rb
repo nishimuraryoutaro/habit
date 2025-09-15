5f6395ddbe5407936f9f7489f7a919e54675e80b
ricardo <nishimu75618094@gmail.com>
2025-09-15 21:16:11 +0900

    日にち表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    redirect_to tasks_path if user_signed_in?
+    raw = params[:date].to_s.strip
+      if raw.match?(/\A\d{4}-\d{2}-\d{2}\z/)
+        y, m, d = raw.split("-").map(&:to_i)
+        @selected_date = Date.valid_date?(y, m, d) ? Date.new(y, m, d) : Date.current
+      else
+        @selected_date = Date.current
+      end
   end
 end
