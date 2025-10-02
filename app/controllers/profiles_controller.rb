2e7d0a8213813b52efdd222f4a2f993ee7c96f82
ricardo <nishimu75618094@gmail.com>
2025-10-02 14:33:32 +0900

    goals destroy and fix create action, calendar validate

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,9 +2,8 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user   = User.find(params[:id])
-    @goal   = @user.goals.order(:created_at).first || @user.goals.build
-    @goals  = @user.goals.order(created_at: :desc)
+    @user   = current_user
+    @goals  = Goal.where(user_id: @user.id).order(created_at: :desc)
   end
 
   def create
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,16 +1,19 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :daily_tasks, dependent: :destroy
-  validates :title, presence: true
   validates :title, :start_date, :target_date, presence: true
+  validate :start_before_target
 
   scope :three_month, -> { where(goal_type: 0) }
   scope :one_month,   -> { where(goal_type: 1) }
 
   private
   def start_before_target
-    if start_date.blank? || target_date.blank? && start_date > target_date
-      errors.add(:start_date, "は目標日より前の日付を指定してください")
+    if start_date.blank? || target_date.blank?
+    else
+      if target_date <= start_date
+        errors.add(:target_date, "は開始日より後の日付にしてください")
+      end
     end
   end
 end
